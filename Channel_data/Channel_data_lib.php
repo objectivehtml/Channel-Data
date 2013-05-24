a66fb1edb0b6851a0c36f7fa0a5ecf408a040532
Justin Kimbrell <justin@objectivehtml.com>
2013-05-23 21:57:47 -0400

    Fixed a rare issue with the get_members() method that would cause it to fail if no matching record was joined. Used LEFT JOIN to be more forgiving.

--- Channel_data/Channel_data_lib.php
+++ Channel_data/Channel_data_lib.php
@@ -1271,7 +1271,7 @@ if(!class_exists('Channel_data_lib'))
 
 			// Joins the channel_data table
 
-			$this->EE->db->join('member_data', 'members.member_id = member_data.member_id');
+			$this->EE->db->join('member_data', 'members.member_id = member_data.member_id', 'left');
 
 			$params = array(
 				'select' 	=> $select,
